44f95acae4f3d5b208be6ba8a6e229e49e49f6df
George Koniaris <koniaris.giwrgos@gmail.com>
2020-04-28 02:20:59 -0700

    Use correct env variable

--- src/services/WordService.ts
+++ src/services/WordService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const fetchWordMeaning = async function(word: string) {
-    const wordData = await axios.get(process.env.DICTIONARY_API_URL + word)
+    const wordData = await axios.get(process.env.REACT_APP_DICTIONARY_API_URL + word)
 
     if (!wordData.data.length) return null
     const wordType = Object.keys(wordData.data[0].meaning)[0]
